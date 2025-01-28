80b52018e46ad66cb5ccddb5181a0209ecdcdf33
sagar gupta <sagarkshn8@gmail.com>
2025-01-28 16:23:18 +0530

    commit fix

--- shop/vite.config.js
+++ shop/vite.config.js
@@ -3,14 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // Allow access to the dev server from other devices
-    port: 5173, // You can set a specific port if needed
-    proxy: {
-      "/api": {
-        target: "http://localhost:9001",
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:9001",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 });
