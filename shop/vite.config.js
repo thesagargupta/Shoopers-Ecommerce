b618d5389a14764d4f62beb98a4fca1aafb5fe06
sagar gupta <sagarkshn8@gmail.com>
2025-01-28 17:29:50 +0530

    commit fix

--- shop/vite.config.js
+++ shop/vite.config.js
@@ -3,12 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:9001",
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
+  build: {
+    outDir: 'build', // This ensures the build directory is created as 'build'
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:9001", // The backend server you're targeting
+        changeOrigin: true, // Modifies the origin of the request to the target URL
+      },
+    },
+  },
 });
